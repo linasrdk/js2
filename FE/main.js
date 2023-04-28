e45ec451e0992a3b65458739f5289c0d394ff5d5
linasrdk <129596449+linasrdk@users.noreply.github.com>
2023-04-28 12:06:13 +0300

    Add files via upload

---
+++ FE/main.js
@@ -0,0 +1,8 @@
+function changeText() {
+  document.getElementById("Input").value = "text";
+}
+
+function showAlert() {
+  var text = document.getElementById("myInput").value ="try again";
+  alert(text);
+}
